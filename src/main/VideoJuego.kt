250b7f8448c690a859b2cf68d84bf5dfc49560fb
StebanSzs <mbravod@unicauca.edu.co>
2024-03-01 15:56:20 -0500

    Retrocediento cambios

--- src/main/VideoJuego.kt
+++ src/main/VideoJuego.kt
@@ -5,56 +5,63 @@ fun main() {
     println()
     println()
 
-    val personajes = listOf(Guerrero("Atreyu"), Hechizero("Merlin"), Guerrero("Goku"), Hechizero("Diablo"), Bruja("Grenilda"))
+    val guerrero1=Guerrero("Atreyu")
+    val hechizero1 = Hechizero("Merlin")
+    val guerrero2=Guerrero("Goku")
+    val hechizero2 = Hechizero("Diablo")
+    val bruja1 = Bruja("Grenilda")
 
     println("***** Selecciona una opción *****")
-    imprimirOpciones("Opciones", listOf("Iniciar juego", "Instrucciones", "Salir"))
+    println()
+    println("1. Iniciar juego")
+    println("2. Instrucciones")
+    println("3. Salir")
 
-    val opcion = readLine()?.toIntOrNull() ?: 0
+    val opcion = readLine()?.toIntOrNull()
 
     when (opcion) {
         1 -> println("Seleccionaste el personaje guerrero")
+
         2 -> println("Seleccionaste el personaje Hechizero")
+
         3 -> println("Seleccionaste el personaje Bruja")
+
         else -> println("Opción no válida")
     }
 
     println("***** Selecciona un personaje *****")
-    val personajeSeleccionado = seleccionarPersonaje("Personajes")
+    println()
+    println("1. Guerrero")
+    println("2. Hechizero")
+    println("3. Bruja")
 
-    when (personajeSeleccionado) {
-        is Guerrero -> println("Seleccionaste el personaje Guerrero")
-        is Hechizero -> println("Seleccionaste el personaje Hechizero")
-        is Bruja -> println("Seleccionaste el personaje Bruja")
+
+    val selecPersonaje = readLine()?.toIntOrNull()
+
+    when (selecPersonaje) {
+        1 -> println("Seleccionaste el personaje guerrero")
+
+        2 -> println("Seleccionaste el personaje Hechizero")
+
+        3 -> println("Seleccionaste el personaje Bruja")
+
         else -> println("Opción no válida")
     }
 
-   /* guerrero1.atacar(guerrero2)
-    guerrero1.atacar(guerrero2)
-    println("${guerrero1.nombre} Ataca a ${guerrero2.nombre}")
-    println(guerrero2.imprimirPersonaje())
 
-    hechizero2.curar(guerrero2)
-    println("${hechizero2.nombre} Cura a ${guerrero2.nombre}")
-    println(guerrero2.imprimirPersonaje())
+    /* guerrero1.atacar(guerrero2)
+     guerrero1.atacar(guerrero2)
+     println("${guerrero1.nombre} Ataca a ${guerrero2.nombre}")
+     println(guerrero2.imprimirPersonaje())
 
-    guerrero2.atacar(guerrero1)
-    println(guerrero1.imprimirPersonaje())
+     hechizero2.curar(guerrero2)
+     println("${hechizero2.nombre} Cura a ${guerrero2.nombre}")
+     println(guerrero2.imprimirPersonaje())
 
-    bruja1.atacar(guerrero1)
-    println(guerrero1.imprimirPersonaje())
-*/
-}
-fun imprimirOpciones(titulo: String, opciones: List<String>) {
-    println("***** $titulo *****")
-    println()
-    opciones.forEachIndexed { index, opcion ->
-        println("${index + 1}. $opcion")
-    }
-}
+     guerrero2.atacar(guerrero1)
+     println(guerrero1.imprimirPersonaje())
 
-fun seleccionarPersonaje(titulo: String): Personaje? {
-    imprimirOpciones(titulo, personajes.map { it.nombre })
-    val seleccion = readLine()?.toIntOrNull()
-    return personajes.getOrNull(seleccion?.minus(1) ?: -1)
+     bruja1.atacar(guerrero1)
+     println(guerrero1.imprimirPersonaje())
+ */
 }
