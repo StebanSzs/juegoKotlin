a9342a1eaacf21964081fbca1cb412595db231e7
StebanSzs <mbravod@unicauca.edu.co>
2024-03-01 17:00:17 -0500

    Cambios en el main, implementación de métodos para los texto y figuras ASCII de el caballero y la Bruja

--- src/main/VideoJuego.kt
+++ src/main/VideoJuego.kt
@@ -1,13 +1,12 @@
 package main
 
 fun main() {
-
     println("**************************************")
     println("***** Inicio del juego Telelocos *****")
     println("**************************************")
 
     print("\nIngresa tu nombre: ")
-    val nombre = readLine()
+    val nombre = readLine().toString()
     println()
 
     var opcion: Int?
@@ -17,60 +16,42 @@ fun main() {
         opcion = readLine()?.toIntOrNull()
 
         when (opcion) {
-            1 -> println("Bienvenido ${nombre.uppercase()} Al juego de los Telelocos\n")
-            2 -> println("Seleccionaste el personaje Hechizero\n")
-            3 -> println("Seleccionaste el personaje Bruja\n")
+            1 -> println("Bienvenido al juego de los Telelocos ${nombre.uppercase()} \n")
+            2 -> println("Instrucciones\n")
+            3 -> imprimirDespedida()
             else -> println("Opción no válida\n")
         }
 
+        if (opcion != 3) {
+            var selecPersonaje: Int?
+
+            do {
+                imprimirTuPersonaje()
+                selecPersonaje = readLine()?.toIntOrNull()
+
+                when (selecPersonaje) {
+                    1 -> imprimirAsciiCaballero()
+                    2 -> println("Seleccionaste el personaje Hechizero\n")
+                    3 -> imprimirAsciiBruja()
+                    else -> println("Opción no válida\n")
+                }
+            } while (selecPersonaje !in 1..3)
+
+            var selecEnemigo: Int?
+
+            do {
+                imprimirTuEnemigo()
+                selecEnemigo = readLine()?.toIntOrNull()
+
+                when (selecEnemigo) {
+                    1 -> println("Tu enemigo es el personaje guerrero")
+                    2 -> println("Tu enemigo es el personaje Hechizero")
+                    3 -> println("Tu enemigo es el personaje Bruja")
+                    else -> println("Opción no válida\n")
+                }
+            } while (selecEnemigo !in 1..3)
+        }
     } while (opcion !in 1..3)
-
-    var selecPersonaje: Int?
-
-    do {
-        imprimirTuPersonaje()
-        selecPersonaje = readLine()?.toIntOrNull()
-
-        when (selecPersonaje) {
-            1 -> println("Seleccionaste el personaje guerrero\n")
-            2 -> println("Seleccionaste el personaje Hechizero\n")
-            3 -> imprimirAsciiBruja()
-            else -> println("Opción no válida\n")
-        }
-
-    } while (selecPersonaje !in 1..3)
-
-    var selecEnemigo: Int?
-
-    do {
-        imprimirTuEnemigo()
-        selecEnemigo = readLine()?.toIntOrNull()
-
-        when (selecEnemigo) {
-            1 -> println("Tu enemigo es el personaje guerrero")
-            2 -> println("Tu enemigo es el personaje Hechizero")
-            3 -> println("Tu enemigo es el personaje Bruja")
-            else -> println("Opción no válida\n")
-        }
-
-    } while (selecEnemigo !in 1..3)
-
-
-    /* guerrero1.atacar(guerrero2)
-     guerrero1.atacar(guerrero2)
-     println("${guerrero1.nombre} Ataca a ${guerrero2.nombre}")
-     println(guerrero2.imprimirPersonaje())
-
-     hechizero2.curar(guerrero2)
-     println("${hechizero2.nombre} Cura a ${guerrero2.nombre}")
-     println(guerrero2.imprimirPersonaje())
-
-     guerrero2.atacar(guerrero1)
-     println(guerrero1.imprimirPersonaje())
-
-     bruja1.atacar(guerrero1)
-     println(guerrero1.imprimirPersonaje())
- */
 }
 fun imprimirMenuOpciones() {
     println("***** Selecciona una opción *****")
@@ -96,44 +77,61 @@ fun imprimirTuEnemigo() {
     println("3. Bruja")
 }
 
+fun imprimirDespedida(){
+    println("**************************************")
+    println("***** Gracias por jugar Telelocos *****")
+    println("**************************************")
+}
+
 fun imprimirAsciiBruja(){
-    println("Seleccionaste el personaje Bruja\n" +
-            "|||||||||||||||||||||||\n" +
-            "    |||||||||||||||||||||||\n" +
-            "    |||||||||||||||||||||||\n" +
-            "    |||||||||||||||||||||||\n" +
-            "    ||||||||||||||||||||||| _._\n" +
-            "    |||||||||||||||||||||||/  _`\\\n" +
-            "    ||||||||||||||||||||||;  /  `\n" +
-            "    |||||||||||||||||||||/   |\n" +
-            "    ||||||||||||||||||||/     \\\n" +
-            "    |||||||||||||||||||/       ;\n" +
-            "    |=================;        |\n" +
-            "    |                 |         \\\n" +
-            "    ;-----------------;          \\\n" +
-            "    \\\\\\||||||||||||||/           `\\\n" +
-            "     \\\\\\||||||||||||/              '.\n" +
-            "      ':\\\\||||||||.'                 `\\\n" +
-            "        `-=;;||||/                     \\\n" +
-            "             _|_/_                      ;\n" +
-            "     __.--\"\"`     ``'-.                 |\n" +
-            "     '.______          `'-._            \\\n" +
-            "            /```--.._       '._          |\n" +
-            "            | )))    `-.       `'._      \\\n" +
-            "            |/_\\`'.     `;-.       '._    ;\n" +
-            "            _)o/_.'       \\\\\\-.       '.  |\n" +
-            "          .'               \\\\\\\\\\;_      '.|\n" +
-            "        _/                 )))))));-._    ``'--._,\n" +
-            "      (`                  ////////////;-..._____.'\n" +
-            "       \\__.-._)          ((((((((((((((((((\\,\n" +
-            "             \\            \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\,\n" +
-            "             (__.-'        \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n" +
-            "              (_.           )))))))))))))))))))))\n" +
-            "               | `\\        (((((((((((((((((((((\n" +
-            "               |  |\\        \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n" +
-            "               |  | '.__     \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n" +
-            "               |  |     '.__/ )))))))))))))))))))))\n" +
-            "               |  |        ) (((((((((((((((((((((\n" +
-            "               |  |      .'   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" +
+    println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡆⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⡿⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣀⣀⣤⣴⣾⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⢀⣴⠆⠀⠻⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠈⠑⠶⢦⣤⡘⢿⣧⡀⠀⠀⠀⠀⠀⠀\n" +
+            "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠛⠛⠛⠻⠿⠿⠿⠿⠿⠿⠿⠛⣻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣾⣿⣿⣇⠀⠀⠀⠀⠀\n" +
+            "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠄⠀⠀⠀⢠⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢻⣿⣿⣿⡄⠀⠀⠀⠀\n" +
+            "⢸⣿⣿⣿⣿⣿⣿⡿⠟⣿⣿⡿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⢤⣄⣠⣴⡶⠃⠀⣠⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀\n" +
+            "⠸⠿⠿⠿⠟⠛⠉⠀⢠⡟⠁⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠈⠉⠁⠀⠀⣴⣿⣿⣿⣿⡄⠀⠀⠢⢴⣶⣶⣄⠀⠀⠀⣸⣿⣿⣿⡇⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⢸⣇⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣤⣴⣾⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠈⠙⢿⣷⣤⣴⣿⣿⣿⣿⡇⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⢀⣀⣤⣤⣈⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀\n" +
+            "⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⡀⠀⠈⠻⣿⣿⣿⣿⡇⠀⠀⠀⠀\n" +
+            "⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⣠⣿⣿⣿⣿⡇⠀⠀⠀⠀\n" +
+            "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣼⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀\n" +
+            "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀\n" +
+            "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀\n" +
+            "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀\n" +
+            "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀\n" +
+            "⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧" +
+            "\n")
+}
+
+fun imprimirAsciiCaballero (){
+    println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣿⣿⣿⣶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⢟⣭⣴⣶⡦⠍⠛⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠈⠳⣶⣤⣤⣶⣿⠿⢫⣾⣿⣿⠋⠀⠀⠀⠀⢸⣿⡟⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠈⠉⠉⠉⠁⣰⣿⣿⣿⠇⠀⢀⣀⣤⣴⣾⣧⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⡟⢠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⣠⣿⣿⣿⡟⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀\n" +
+            "⠀⠀⢀⣠⣶⣿⣿⡿⠋⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⠀⠀⠀⠀\n" +
+            "⠉⠛⠛⠛⠛⠛⠉⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣛⣥⣶⣆⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⠏⣥⣤⡙⢟⣫⡴⠿⠿⠿⠷⠿⣷⡀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⡜⢿⡿⢃⣌⢻⣟⠛⠻⠶⠶⢶⣾⣿⡄\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⡄⣾⣿⣿⣷⡝⢿⣷⣶⣶⣦⡾⠟⠁\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣟⣛⣻⠿⠿⢧⢹⣿⣿⣿⣿⣦⡙⢷⡶⠋⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣝⠻⣿⣿⣛⠷⠌⢿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣮⣝⠻⣿⣶⣦⣤⣉⠛⠿⢿⠁⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠰⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣛⠿⢿⣧⢷⣤⡀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠿⣿⣿⣿⣿⣷⡦⠉⢿⣿⡷⠦⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⡿⠋⠀⠀⠈⠀⠀⠀⠀⠀⠀" +
             "\n")
 }
